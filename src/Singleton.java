dc92fd3730c06c00e55893d430f469737b3a7510
dsouzarc <dsouzarc@gmail.com>
2014-08-15 10:03:13 -0400

    Added counter

--- src/Singleton.java
+++ src/Singleton.java
@@ -5,9 +5,13 @@ public class Singleton {
     //The instance of a class
     private static Singleton theInstance = null;
     
+    //Count number of instances created
+    private static int numCreated;
+    
     /** Hidden constructor */
     private Singleton() { 
-        
+        numCreated++;
+        System.out.println("Singleton Constructor\t" + numCreated);
     }
     
     /** Returns an instance of the class */
